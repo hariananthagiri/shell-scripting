a6b7b6fef6dc56f0e5da38f070891ae8a4c9aaf8
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-16 07:25:53 +0530

    #using arguments in varibles

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#using arguments
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: hi how are you ??"
+echo "$PERSON2: iam doing good thank you..."
+echo "$PERSON2: how are you $PERSON1"
+echo "$PERSON1: iam doing good thank you..."
