3755b179504df70947c72fb3b7be74a68579680f
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-17 10:39:35 +0530

    #using arguments in varibles

--- 15-colours.sh
+++ 15-colours.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-R=$(\e[31m)
-G=$(\e[32m)
-Y=$(\e[33m)
-N=$(\e[0m)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-echo -e "iam $Y using shell scripting $G sucess $R fail $N narmal"
+echo -e "iam using $Y shell scripting $G sucess $R fail $N narmal"
