a7cd8127fd05003ce048125813f547419defc102
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-17 21:42:05 +0530

    #using arguments in varibles

--- 17-functions.sh
+++ 17-functions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 hari.function(){
-if [$1 -gt $2]
-then
-   echo "$1 is bigger"
-else
-   echo "$2 is bigger"
-fi 
+   if [$1 -gt $2]
+   then
+      echo "$1 is bigger"
+   else
+      echo "$2 is bigger"
+   fi 
+
 }
 hari.function 10 20
