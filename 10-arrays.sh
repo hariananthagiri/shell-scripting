094f6958bf5ab471cfab00ff160874e87ba3899f
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-17 05:33:18 +0530

    #using arguments in varibles

---
+++ 10-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[1]}"
+
+echo "Second value: ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
