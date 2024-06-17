c8ba83b5f45b38f5a2c904a9b1fd3a1222a7f9b6
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 05:29:39 +0530

    #using arguments in varibles

---
+++ 18-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# we can store a file in variable instad of storeing value
+# we can give a particular location to save the file 
+
+NAME="/C/Users/haria/github/shivaananthagiri/repos/shell-script/hari.txt"
+NAME=HARI KRISHNA
+cat /C/Users/haria/github/shivaananthagiri/repos/shell-script/hari.txt
