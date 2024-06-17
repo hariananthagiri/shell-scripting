4d519ced65c8cfede4f795d84dba700657865ccb
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-17 08:22:30 +0530

    #using arguments in varibles

--- 14-logfile.sh
+++ 14-logfile.sh
@@ -3,3 +3,6 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 yum install mysql -y &>> $LOGFILE
+
+# we will get error as shown below
+# This command has to be run with superuser privileges (under the root user on most systems).
