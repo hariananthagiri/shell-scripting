ba7feeb335ebff59dc6f57965381e40e686a68e5
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 06:27:38 +0530

    #using arguments in varibles

--- 18-variables.sh
+++ 18-variables.sh
@@ -3,7 +3,7 @@
 # we can store a file in variable instad of storeing value
 # we can give a particular location to save the file 
 
-NAME="/shell-script/hari.txt"
+NAME="/home/centos/shell-script/hari.txt"
 echo "Iam Storeing the value of variable in file" > $NAME 
 
 
