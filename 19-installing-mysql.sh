bf6c8cad08b9ea695a00345bf4b79f89b4c3071d
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 09:16:46 +0530

    #using arguments in varibles

---
+++ 19-installing-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/e[0m"
+ID=$(id -u)
+
+if [ ID -ne 0 ]
+then
+   echo " $R ERROR $N your not a root user"
+else
+   echo "your a root user"
+fi
+
