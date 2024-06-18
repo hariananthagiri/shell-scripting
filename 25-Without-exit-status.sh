d9574a826a9d1488f0e46c6c514f90101bbe19f1
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 19:48:48 +0530

    #using arguments in varibles

---
+++ 25-Without-exit-status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e # by using this command it will stop the code exection from when it is failed.
+touch example.txt
+ls -l
+echo " before wrong command "
+hhhhhh
+echo " After wrong command "
