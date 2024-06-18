31bfd79c3845c6b4bdfe083d176459f8fbcdb33b
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 06:41:41 +0530

    #using arguments in varibles

--- 18-variables.sh
+++ 18-variables.sh
@@ -4,6 +4,8 @@
 # we can give a particular location to save the file 
 
 NAME="/home/centos/shell-script/hari.txt"
+# /home/centos/shell-script give the path of vartual meachine or serverbecause we are executeing the code in server.
+# $<variable_name> ---> stores the value n varaible and reflect the value where we need and when we need
 echo "Iam Storeing the value of variable in file" > $NAME 
 
 
