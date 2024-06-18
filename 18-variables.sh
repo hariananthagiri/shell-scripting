da75919e7cc0a49045ca8601af1dab8367208413
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 06:24:18 +0530

    #using arguments in varibles

--- 18-variables.sh
+++ 18-variables.sh
@@ -3,8 +3,8 @@
 # we can store a file in variable instad of storeing value
 # we can give a particular location to save the file 
 
-NAME="/tmp/hari.txt"
-yum install git -y &> $NAME
+NAME="/C/Users/haria/github/shivaananthagiri/repos/shell-script/hari.txt"
+echo "Iam Storeing the value of variable in file" > $NAME 
 
 
 
