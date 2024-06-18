c3bcb85c5e92018fcb0b86f4fc86800944a3d6d7
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 05:57:00 +0530

    #using arguments in varibles

--- 18-variables.sh
+++ 18-variables.sh
@@ -3,7 +3,7 @@
 # we can store a file in variable instad of storeing value
 # we can give a particular location to save the file 
 
-NAME="/C/Users/haria/github/shivaananthagiri/repos/shell-scripting/hari.txt"
+NAME="/tmp/hari.txt"
 echo "storeing variable value in afile" &> NAME
 
 
