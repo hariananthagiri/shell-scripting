eb8004e1313ee8b1f63b4851e1ef903ea36e4740
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 10:26:35 +0530

    #using arguments in varibles

---
+++ 21-installing-functions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ID=$(id -u)
+
+validate1(){
+if [ $ID -ne 0 ]
+then
+   echo -e " $R ERROR::$N your not a root user "
+   exit 1
+else
+   echo -e " $G your a root user $N "
+fi
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+   echo -e " $R ERROR::$N installing mysql is failed "
+   exit 1
+else
+   echo -e " $G Installing MYSQL IS SUCESSFUL "
+fi
+}
+validate2(){
+if [ $? -ne 0 ]
+then
+   echo -e " $R ERROR::$N installing $1 is failed "
+   exit 1
+else
+   echo -e " $G Installing $1 IS SUCESSFUL "
+fi    
+}
+
+# now installing packages
+
+validate1
+yum install mysql -y
+validate2 MYSQL
+
+validate1
+yum install git -y
+validate2 GIT
+
+
