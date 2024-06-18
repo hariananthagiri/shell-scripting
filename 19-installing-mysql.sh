e90ceeaa7f77848114db5fdbfa79b7364bb6c0ea
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 10:09:37 +0530

    #using arguments in varibles

--- 19-installing-mysql.sh
+++ 19-installing-mysql.sh
@@ -22,3 +22,16 @@ then
 else
    echo -e " $G Installing MYSQL IS SUCESSFUL "
 fi
+
+yum install git -y
+
+# here we are instaliing mysql and git 2 packages are installing and same code is repeating multipule times
+# to over come that function concept is came refer pgm 21-installing-functions.sh
+
+if [ $? -ne 0 ]
+then
+   echo -e " $R ERROR::$N installing GIT is failed "
+   exit 1
+else
+   echo -e " $G Installing GIT IS SUCESSFUL "
+fi
