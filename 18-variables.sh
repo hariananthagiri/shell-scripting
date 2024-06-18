665ce699eae9e907ec4576dbaf43e224946e1178
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-18 06:06:06 +0530

    #using arguments in varibles

--- 18-variables.sh
+++ 18-variables.sh
@@ -4,6 +4,6 @@
 # we can give a particular location to save the file 
 
 NAME="/tmp/hari.txt"
-echo "storeing variable value in afile" &> NAME
+echo "storeing variable value in afile in it" &> NAME
 
 
