9181862ad165da4ce11a9709891750c39a002f41
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-19 15:07:27 +0530

    #using arguments in varibles

--- 30-using-awk-cut.sh
+++ 30-using-awk-cut.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 file=$(cat file1.txt)
+condition=$(awk -F : 'print $1F $2F $3F' | cut -d % f3)
  echo "$file"
+
+ while IFS= read -r line
+ do
+ $condition
+ echo "$condition"
+ echo "$line"
+ done <<< $file
