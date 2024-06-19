ee89edfe842bcbedeb37496d7a143aceba369b6f
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-19 09:06:31 +0530

    #using arguments in varibles

--- 28-IFS.sh
+++ 28-IFS.sh
@@ -11,18 +11,21 @@ file="/etc/passwd"
    echo -e "$Y $file $R diectory is not available $N"
  else
     echo -e "$Y $file $G diectory is available $N"
- fi   
-# we are using INTERNAL FIELD SAPERATER [ i.e , . & @ any special charector ] it give row wise information
+ fi
+    
+# we are using INTERNAL FIELD SAPERATER [ i.e , . & @ any special charector ] it give row to column wise information
 
-while IFS=":"  read -r user_name password user_id group_id
+while IFS=":"  read -r user_name password user_id group_id user_full_name home_dir shell_path
 do
    echo -e "uer_name:$user_name" 
    echo -e "password:$password"
    echo -e "user_id:$user_id" 
    echo -e "group_id:$group_id"
-
-
+   echo -e "user_full_name $user_full_name"  
+   echo -e "home_dir:$home_dir" 
+   echo -e "shell_path:$shell_path"           
+do
 done < $file
 
-# it take output of DELETEING_FILE variable as input to while loop if we give <<< thress less than symbols
+# it take output of file variable as input to while loop 
 # IFS=read -r read line by line and stores in line variable                     
