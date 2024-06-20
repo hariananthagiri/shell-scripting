a073f72646c6034eba6b516aae8ece870b8ccea6
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-20 05:46:51 +0530

    #using arguments in varibles

--- 33-disk-usage.sh
+++ 33-disk-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File' | awk '{print $6F}' | cut -d % -f1)
-partition=$(echo $line | awk '{print $1F}')
+partition=$(awk '{print $1F}')
 DISK_THRESHOLD=1
 message=""
 
