ecf4d408e644477fcb3fb7b768fc4d1ed032ff75
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-20 05:39:47 +0530

    #using arguments in varibles

---
+++ 33-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File' | awk '{print $6F}' | cut -d % -f1)
+partition=$(echo $line | awk '{print $1F}')
+DISK_THRESHOLD=1
+message=""
+
+while IFS= read line
+do 
+   echo $line
+
+   if [ $line -ge $DISK_THRESHOLD ]
+   then
+      message+="high disk usage on $partition:$line\n"
+   fi
+done <<< $DISK_USAGE
+
+echo -e "message is:$message"
+
