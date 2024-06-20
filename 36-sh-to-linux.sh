eefb00c20d7d971ce9449b454c52a3229b25b638
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-20 21:19:29 +0530

    #using arguments in varibles

---
+++ 36-sh-to-linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Converting a .sh file to linux command
+# $PATH
+# by using above command you can see the paths available in existing directory
+# select usr/local/bin/<file-name> and copy our .sh file here
+# sudo cp <file-name.sh> usr/local/bin/<file-name>
+# now giva execute command to that file
+# sudo chmod +x usr/local/bin/<file-name>
+# now exexute <file-name> anywhere in linux it works as linux command
+ echo " Converting a .sh file to linux command "
+ 
