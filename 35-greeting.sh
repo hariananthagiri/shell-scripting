e3f7fd51594b3e9d103bce0603ad94e4de73df4c
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-20 17:51:55 +0530

    #using arguments in varibles

---
+++ 35-greeting.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+   echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+   echo "OPTIONS::"
+   echo "-n, specify a name"
+   echo "-w, specify a wishesh"
+   echo " -h, Display help and exit"
+}
+
+while getopts "n:w:h" opt; do
+   case $opt in
+      n) NAME="$OPTARG";;
+      w) WISHES="$OPTARG";;
+      h|*) USAGE;exit;;
+   esac
+done
+
+echo "hii hello $NAME,$WISHES"
