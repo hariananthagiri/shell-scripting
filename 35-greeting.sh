6a34502926bfc0b164b89bc215f9a5ae6d99506b
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-21 06:59:29 +0530

    #using arguments in varibles

--- 35-greeting.sh
+++ 35-greeting.sh
@@ -15,9 +15,17 @@ while getopts ":n:w:h:" opt; do
    case $opt in
       n) NAME="$OPTARG";;
       w) WISHES="$OPTARG";;
-      h|*) USAGE;exit;;
+      h) USAGE;exit;;
       :) USAGE;exit;;
+      \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
    esac
 done
 
+if [ -z "$NAME" ] || if [ -z "$WISHES" ]
+then
+   echo "ERROR:both -n and -w are mandatory"
+   USAGE
+   exit 1
+fi    
+ 
 echo -e "hii hello $NAME. $WISHES i have been learning shell scripting"
