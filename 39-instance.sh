d028a646d24fcca18d345495a5a6995ff1145ea0
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-22 10:38:25 +0530

    #using arguments in varibles

--- 39-instance.sh
+++ 39-instance.sh
@@ -13,7 +13,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     
-    echo "$i is INSTALLING....."
+    echo "$i is INSTALLING...."
 
     aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
     
