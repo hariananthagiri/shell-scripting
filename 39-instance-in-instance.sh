0e660be71df33b701650ff7eb29ead92c7c206cc
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-22 06:49:30 +0530

    #using arguments in varibles

--- 39-instance-in-instance.sh
+++ 39-instance-in-instance.sh
@@ -7,7 +7,7 @@ AMI_ID="ami-0b4f379183e5706b9"
 SG_ID="sg-06895fd55cd10cc75"
 INSTANCES=("MYSQL" "MONGODB" "WEB" "CATALOGUE")
 
-for i in ${INSTANCES[@]}
+for i in "${INSTANCES[@]}"
 do
 
    if [ $i == "MYSQL" ] || if [ $i == "MONGODB" ]
@@ -15,8 +15,8 @@ do
       INSTANCE_TYPE="t3.small"
    else
       INSTANCE_TYPE="t2.micro"  
-   fi    
-
-    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+   fi   
+    
     echo " $i is INSTALLING... " 
+    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
