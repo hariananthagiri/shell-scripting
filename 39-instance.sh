af1aeba5210f225b85bd1a9e0fa5154356fb1587
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-22 08:16:59 +0530

    #using arguments in varibles

--- 39-instance.sh
+++ 39-instance.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# we will create instances by using instance here
-# we create a instance first and assigned a role for it.
+AMI=ami-0b4f379183e5706b9 #this keeps on changing
+SG_ID=sg-06895fd55cd10cc75 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
-AMI_ID=ami-0b4f379183e5706b9
-SG_ID=sg-06895fd55cd10cc75
-INSTANCES=("MYSQL" "MONGODB" "WEB" "CATALOGUE")
- 
-for i in ${INSTANCES[@]}
+for i in "${INSTANCES[@]}"
 do
-    if [ $i == "MYSQL" ] || if [ $i == "MONGODB" ]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
-       INSTANCE_TYPE=t3.small
+        INSTANCE_TYPE="t3.small"
     else
-       INSTANCE_TYPE=t2.micro
+        INSTANCE_TYPE="t2.micro"
     fi
-        aws ec2 run-instanceses --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids
-         sg-06895fd55cd10cc75 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-             
-     echo "$i is installing"  
+
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-06895fd55cd10cc75 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    echo "$i"
+
+    
 done
