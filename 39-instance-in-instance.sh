1ee357a2b43dc67250f01d01e15dc77296eddd47
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-22 06:55:31 +0530

    #using arguments in varibles

--- 39-instance-in-instance.sh
+++ 39-instance-in-instance.sh
@@ -18,5 +18,5 @@ do
    fi   
     
     echo " $i is INSTALLING... " 
-    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"&> /tmp/$0.log
 done
