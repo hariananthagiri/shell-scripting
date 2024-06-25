6b7bf8330987fcb6ce26bf8351f8c71598d4df9a
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-25 08:32:50 +0530

    ansible example

--- wildcars.sh
+++ wildcars.sh
@@ -9,9 +9,9 @@ cat names.txt | grep "man*" | wc -l
 
 # ? --> we use this we dont know some words of text and we use ? in unknown words
 
-echo "using ? wild card"
+echo "using ? wild card wehave use full text"
 
-cat names.txt | grep "har?" | wc -l
+cat names.txt | grep "ha?i" | wc -l
 
 echo "using ^ wild card"
 
@@ -21,9 +21,10 @@ cat names.txt | grep "^h" | wc -l
 
 echo "using $ wild card"
 
-# $ if we known the last letter of the text
+# $ if we known the last letter of the text there
 
-cat names.txt | grep "$u" | wc -l
+cat names.txt | grep "u$" | wc -l
+
 
 
 
