de2497cf217826d48d2c84e7f79cb309167c2c4f
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-25 08:20:36 +0530

    ansible example

--- wildcars.sh
+++ wildcars.sh
@@ -2,20 +2,20 @@
  
 # *--> if we want to search a file we have less information we use this
 
-cat names.txt | grep "*hna" 
-cat names.txt | grep "man*"
+cat names.txt | grep "*hna" | wc -l 
+cat names.txt | grep "man*" | wc -l
 
 # ? --> we use this we dont know some words of text and we use ? in unknown words
 
-cat names.txt | grep "ha?"
+cat names.txt | grep "ha?" | wc -l
 
 # ^ if we known the fist letter of the text
 
-cat names.txt | grep "^h"
+cat names.txt | grep "^h" | wc -l
 
 # $ if we known the last letter of the text
 
-cat names.txt | grep "$u"
+cat names.txt | grep "$u" | wc -l
 
 
 
