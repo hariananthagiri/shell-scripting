05eb17d60601576215efe14d7ed5340ed1bc9c26
shivaananthagiri <shiva.ananthagiri12507.com>
2024-06-25 07:47:50 +0530

    ansible example

--- redirection.sh
+++ redirection.sh
@@ -9,9 +9,9 @@ ls -l > output.txt # here the output is stored to file1.txt
 echo "hari krishna" >> output.txt   # here the output is stored and appended to file1.txt
 
 wc -l < output.txt  # it will take the input as data from output.txt file and give to wc command
-wc -l;wc -c << output.txt  # it will append the take  input from output.txt file and give to wc command
+wc -l; wc -c << output.txt  # it will append the take  input from output.txt file and give to wc command
 
 wc -l 1> sucess.txt # it will store only sucessfull output to sucess.txt file
 wrong 2> error.txt  # it will store only failure output to errors.txt file     
-ls -l; hhhh &> all.txt  # it will store both sucess and failure output in all.txt
+hhhh &> all.txt     # it will store both sucess and failure output in all.txt
 
